164c8fee12ce1ade46667f21894ccca893d6ce0d
Manish Shelar <shelarmanish14@gmail.com>
2026-07-13 00:16:48 +0530

    Lab8_Q2.py
    2.Write a Python script to concatenate the following dictionaries to create a new one.
    
    Sample Dictionary : dic1={1:10, 2:20} dic2={3:30, 4:40} dic3={5:50,6:60}
    
     Expected Result : {1: 10, 2: 20, 3: 30, 4: 40, 5: 50, 6: 60}

---
+++ Lab8_Q2.py
@@ -0,0 +1,22 @@
+"""Program: .Write a Python script to concatenate the following dictionaries to create a new one.
+
+Sample Dictionary : dic1={1:10, 2:20} dic2={3:30, 4:40} dic3={5:50,6:60}
+
+ Expected Result : {1: 10, 2: 20, 3: 30, 4: 40, 5: 50, 6: 60}
+ """
+
+dic1 = {1:10, 2:20}
+dic2 = {3:30, 4:40}
+dic3 = {5:50, 6:60}
+
+new_dict = {}
+
+new_dict.update(dic1)
+new_dict.update(dic2)
+new_dict.update(dic3)
+
+print(new_dict)
+
+
+ #Output:-
+# {1: 10, 2: 20, 3: 30, 4: 40, 5: 50, 6: 60}
