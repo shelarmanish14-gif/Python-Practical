5864a6d8ed7cd29d6e78af7bedbe9dea882c7081
Manish Shelar <shelarmanish14@gmail.com>
2026-07-13 00:13:38 +0530

    Lab7_Q1.py
    1. Write a Python program to sum all the items in a list.

---
+++ Lab7_Q1.py
@@ -0,0 +1,20 @@
+# Program:. Write a Python program to sum all the items in a list. 
+
+# Create a list
+list1 = [10, 20, 30, 40, 50]
+
+# Variable to store sum
+total = 0
+
+# Add each item
+for i in list1:
+    total = total + i
+
+# Display result
+print("List:", list1)
+print("Sum of all items:", total)
+
+
+#output:-
+# List: [10, 20, 30, 40, 50]
+# Sum of all items: 150
